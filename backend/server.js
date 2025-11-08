0b3be5d37fbc6fe7bf22d4f6571c0b10307e0ccf
ashishpundir1997 <ashishpundir1997@gmail.com>
2025-11-08 22:37:29 +0530

    change in backend

--- backend/server.js
+++ backend/server.js
@@ -16,11 +16,17 @@ connectDB();
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:3000', 'http://localhost:5174, "https://project-manager-blue-psi.vercel.app/'],
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
+    origin: [
+      'http://localhost:5173',
+      'http://localhost:3000',
+      'http://localhost:5174',
+      'https://project-manager-blue-psi.vercel.app'
+    ],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+  }));
+  
 app.use(express.json());
 
 // Routes
